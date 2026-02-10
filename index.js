4b929ab6cc6c6bd39cc250dbeaee2df19905fb61
Mohammad Ali <muhammadali.pass1@gmail.com>
2026-02-10 06:31:26 +0600

    Connected to mongodb atlus

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require("express")
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Simple crud server in running")
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onnu8qm.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, ()=> {
     console.log(`Simple crud server in running on port: ${port}`)
