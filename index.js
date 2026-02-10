299b2aee035a633ccec0230ce547b77ae6dea692
Mohammad Ali <muhammadali.pass1@gmail.com>
2026-02-10 07:36:35 +0600

    Create user post API to get data form client side

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const express = require("express")
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require("dotenv").config()
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onnu8qm.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
-  }
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!",
+        );
+
+        // API for getting user
+        app.post("/users", (req, res) => {
+            const newUser = req.body
+            console.log(newUser)
+        });
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-app.listen(port, ()=> {
-    console.log(`Simple crud server in running on port: ${port}`)
-}) 
+app.listen(port, () => {
+    console.log(`Simple crud server in running on port: ${port}`);
+});
